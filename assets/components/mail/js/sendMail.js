94ee331909917d97fbbcea41c6f1dbc82a22a5f0
Orintas <orintas@rambler.ru>
2014-02-27 19:55:29 +0400

    Реализована форма обратной связи.

--- assets/components/mail/js/sendMail.js
+++ assets/components/mail/js/sendMail.js
@@ -4,13 +4,28 @@
  * Created by Orintas on 26.02.14.
  */
 angular.module('sendMailMdl', ['service']).
-   controller('contactFormCtrl', ['$scope', 'SendMail', function($scope, SendMail) {
-      $scope.sendMail = function() {
-         console.log("Send mail");
-         SendMail.save({data: 'testData'}, function (response) {
-            $scope.sendMailResponse = response;
-         }, function (response) {
-            $scope.error = response;
-         });
+   controller('contactFormCtrl', ['$rootScope', '$scope', 'SendMail', function($rootScope, $scope, SendMail) {
+      $scope.mail = {
+         isSent: false,
+         error: null,
+         response: null
+      };
+      $scope.mail.isSent = false;
+      var mailSendHandler = function (response) {
+         $scope.mail.isSent = true;
+         $scope.mail.error = response.error;
+         $scope.mail.response = response.message;
+         $rootScope.isLoading = false;
       }
+      $scope.sendMail = function() {
+         $rootScope.isLoading = true;
+         $scope.mail.error = null;
+         $scope.mail.response = null;
+         SendMail.save($scope.mail, mailSendHandler, function (response) {
+            response.error = 'Ошибка отправки сообщения!';
+            mailSendHandler(response);
+         });
+      };
+
+
    }]);
--- core/components/mail/elements/snippets/send_mail.php
+++ core/components/mail/elements/snippets/send_mail.php
@@ -1,4 +1,17 @@
 <?php
-$params = json_decode(file_get_contents('php://input'));
-return json_encode(array("test" => 'Ok', 'data' => $params->data));
+$data = json_decode(file_get_contents('php://input'));
+
+$modx->getService('mail', 'mail.modPHPMailer');
+$modx->mail->set(modMail::MAIL_BODY, $data->text . '<p>Телефон: ' . $data->phone . '</p><p>Email: ' . $data->email);
+$modx->mail->set(modMail::MAIL_FROM, $data->email);
+$modx->mail->set(modMail::MAIL_FROM_NAME, $data->fio);
+$modx->mail->set(modMail::MAIL_SUBJECT, 'Новое сообщение с сайта protsvetnoy.com!');
+$modx->mail->address('to','info@protsvetnoy.com');
+$modx->mail->setHTML(true);
+if (!$modx->mail->send()) {
+    $modx->log(modX::LOG_LEVEL_ERROR,'An error occurred while trying to send the email: '.$modx->mail->mailer->ErrorInfo);
+    return json_encode(array("error" => 'Ошибка отрправки сообщения! Вы можете связаться с нами по телефону или попробовать позднее.'));
+}
+$modx->mail->reset();
+return json_encode(array("message" => 'Ваше сообщение было отправленно! Мы обязательно ответим Вам.'));
 ?>
